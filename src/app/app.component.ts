70f47dbc48f0a4050011a9c9de014f0e14ff64ed
houcem <houcem106@gmail.com>
2022-07-29 20:16:58 +0100

    add communt to understand git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,8 +14,15 @@ export class AppComponent {
   title = 'My To Do List APP'
 }
 
+// ToDo : for houssem brahim
 
 // eventEmitter ==> eventEmitter
 // localStorage
 // ngClass
+
 // github 
+// 1) git init (initialisation)  pour ajouter el git a notre project 
+// 2) git remote (lien fil githib ) <--> (projet fi locale)
+// 3) git  add . (git[espace]add[espace]point)
+// 4) git commit -m 'label' (label : ach 5damt exactement)
+// 5) git push origin master (esm el branche)
